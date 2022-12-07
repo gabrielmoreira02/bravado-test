89894ea0c0c6d6793076aeb6d54f73065d38843a
gabrielmoreira02 <moreiragabriel.aires@gmail.com>
2022-12-06 21:04:46 -0300

    fixing layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,75 +1,24 @@
 <template>
-  <div>
-    <nav
-      class="navbar header has-shadow is-primary"
-      role="navigation"
-      aria-label="main navigation"
-    >
-      <div class="navbar-brand">
-        <a
-          class="navbar-item"
-          href="/"
-        >
-          <img
-            src="~assets/buefy.png"
-            alt="Buefy"
-            height="28"
-          >
-        </a>
-
-        <div class="navbar-burger">
-          <span />
-          <span />
-          <span />
-        </div>
-      </div>
-    </nav>
-
-    <section class="main-content columns">
-      <aside class="column is-2 section">
-        <p class="menu-label is-hidden-touch">
-          General
-        </p>
-        <ul class="menu-list">
-          <li
-            v-for="(item, key) of items"
-            :key="key"
-          >
-            <NuxtLink
-              :to="item.to"
-              exact-active-class="is-active"
-            >
-              <b-icon :icon="item.icon" /> {{ item.title }}
-            </NuxtLink>
-          </li>
-        </ul>
-      </aside>
-
-      <div class="container column is-10">
-        <Nuxt />
-      </div>
-    </section>
-  </div>
+  <section class="main-page-layout is-flex is-justify-content-center is-align-items-center">
+    <div class="page-container">
+      <Nuxt />
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: 'DefaultLayout',
-  data () {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' }
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' }
-        }
-      ]
-    }
-  }
+  name: 'DefaultLayout'
 }
 </script>
+
+<style lang="scss" scoped>
+.main-page-layout {
+  background-color: #EEEEEE;
+  min-height: 100vh;
+}
+.page-container {
+  background-color: #fff;
+}
+
+</style>
